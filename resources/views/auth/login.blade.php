e980474604d0b811631991291462411711899502
Amin Soltani <asoltani927@gmail.com>
2021-05-12 11:41:24 +0430

    Implmenetation guest layout and login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,4 +2,34 @@
 
 @section('content')
     
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">فرم زیر را تکمیل کنید و ورود بزنید</p>
+
+    <form action="../../index2.html" method="post">
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" placeholder="ایمیل">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="رمز عبور">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> مرا به خاطر بسپار
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-12">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">ورود</button>
+        </div>
+        <!-- /.col -->
+      </div>
+    </form>
+
+  </div>
 @endsection
