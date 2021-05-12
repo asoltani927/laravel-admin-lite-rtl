5a1d9a6a53731e93bc8b4a81d341c8bd9df387a4
Amin Soltani <asoltani927@gmail.com>
2021-05-12 11:50:40 +0430

    Changes to be committed: 	modified:   resources/views/app/dashboard.blade.php 	modified:   resources/views/layouts/app.blade.php 	new file:   resources/views/sections/footer.blade.php 	new file:   resources/views/sections/header.blade.php 	new file:   resources/views/sections/leftbox.blade.php 	new file:   resources/views/sections/sidebar.blade.php

--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -2,4 +2,48 @@
 
 @section('content')
     
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <h1>
+          سایدبار
+          <small>نمونه سایدبار بسته</small>
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="#"><i class="fa fa-dashboard"></i> خانه</a></li>
+          <li><a href="#">لایه ها</a></li>
+          <li class="active">سایدبار بسته</li>
+        </ol>
+      </section>
+  
+      <!-- Main content -->
+      <section class="content">
+        <div class="callout callout-info">
+          <h4>توجه</h4>
+  
+          <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+        </div>
+        <!-- Default box -->
+        <div class="box">
+          <div class="box-header with-border">
+            <h3 class="box-title">عنوان</h3>
+  
+            <div class="box-tools pull-right">
+              <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse">
+                <i class="fa fa-minus"></i></button>
+              <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
+                <i class="fa fa-times"></i></button>
+            </div>
+          </div>
+          <div class="box-body">
+            محتوای باکس
+          </div>
+          <!-- /.box-body -->
+          <div class="box-footer">
+            انتها
+          </div>
+          <!-- /.box-footer-->
+        </div>
+        <!-- /.box -->
+      </section>
+      <!-- /.content -->
 @endsection
---
+++ resources/views/sections/footer.blade.php
@@ -0,0 +1,5 @@
+
+  <!-- /.content-wrapper -->
+  <footer class="main-footer text-left">
+    <strong>Copyleft &copy; 2014-2017 <a href="https://adminlte.io">Almsaeed Studio</a> & <a href="http://hosseinizadeh.ir/adminlte">Alireza Hosseinizadeh</a></strong>
+  </footer>
